855319cb050889b43f6b971748d54e6aba80a99b
Daniel Torres <denial.torres@gmail.com>
2022-10-27 11:21:42 -0500

    add page engine class

---
+++ lib/paginate/page.rb
@@ -0,0 +1,38 @@
+module Paginate
+  class Page
+    PER_PAGE = 30
+
+    attr_reader :number, :collection
+
+    def initialize(number, collection)
+      @number = number
+      @collection = collection
+    end
+
+    def records
+      collection.offset(offset).limit(PER_PAGE)
+    end
+
+    def first?
+      number == 1
+    end
+
+    def last?
+      number > (collection.size.to_f / PER_PAGE).ciel
+    end
+
+    def previous_param
+      number - 1
+    end
+
+    def next_param
+      number + 1
+    end
+
+    private
+
+    def offset
+      PER_PAGE * (number - 1)
+    end
+  end
+end
